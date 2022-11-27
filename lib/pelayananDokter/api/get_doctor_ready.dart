ebb67cdb99a3508c98b714699c3392bdd1975d40
yudiptr <94793172+yudiptr@users.noreply.github.com>
2022-11-27 18:26:16 +0700

    fixed http get, form pelayanan Dokter

--- lib/pelayananDokter/api/get_doctor_ready.dart
+++ lib/pelayananDokter/api/get_doctor_ready.dart
@@ -2,8 +2,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:f11uihealcast/pelayananDokter/model/doctor_ready.dart';
 
-List<DoctorReady> _dataDokter = [];
-
 Future<List<DoctorReady>> fetchDokter() async {
   var url = Uri.parse('http://10.0.2.2:8000/pelayananDokter/show_doc/');
   var response = await http.get(
@@ -17,18 +15,12 @@ Future<List<DoctorReady>> fetchDokter() async {
   var data = jsonDecode(utf8.decode(response.bodyBytes));
 
   // melakukan konversi data json menjadi object ToDo
-  _dataDokter = [];
+  List<DoctorReady> _dataDokter = [];
 
   for (var d in data) {
     if (d != null) {
       _dataDokter.add(DoctorReady.fromJson(d));
     }
-
-    print(_dataDokter);
   }
-}
-
-List<DoctorReady> getDokter() {
-  print(_dataDokter);
   return _dataDokter;
 }
--- lib/pelayananDokter/component/form_pelayanan_dokter.dart
+++ lib/pelayananDokter/component/form_pelayanan_dokter.dart
@@ -15,85 +15,100 @@ class _FormPelayananDokter extends State<FormPelayananDokter> {
   get formKey => widget.formKey;
 
   String? _selectDokter;
-  List<DoctorReady>? isReady;
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: SingleChildScrollView(
-          child: Container(
-        margin: const EdgeInsets.all(8),
-        padding: const EdgeInsets.all(20.0),
-        child: Column(children: [
-          Center(
-            child: TextFormField(
-              maxLines: 10,
-              decoration: InputDecoration(
-                hintText: "Masukkan Keluhan Anda...",
-                labelText: "Keluhan",
-                icon: const Icon(Icons.title),
-                // Menambahkan icon agar lebih intuitif
-                // Menambahkan circular border agar lebih rapi
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Masukkan Nomor Telepom",
-                labelText: "No. HP",
-                icon: const Icon(Icons.phone),
-                // Menambahkan icon agar lebih intuitif
-                // Menambahkan circular border agar lebih rapi
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: DropdownButton(
-                value: isReady?[0],
-                hint: const Text("Pilih Jenis"),
-                icon: const Icon(Icons.keyboard_arrow_down),
-                items: isReady!
-                    .map<DropdownMenuItem<DoctorReady>>((DoctorReady value) {
-                  return DropdownMenuItem(
-                    value: value,
-                    child: Text(value.username),
-                  );
-                }).toList(),
-                onChanged: (DoctorReady? value) {
-                  setState(() {
-                    if (value?.username == null) {
-                      _selectDokter = "";
-                    } else {
-                      _selectDokter = value?.username;
-                    }
-                  });
-                },
-              ),
-            ),
-          ),
-        ]),
-      )),
-    );
+    return FutureBuilder(
+        future: fetchDokter(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (!snapshot.hasData) {
+              return Column(
+                children: const [
+                  Text(
+                    "Tidak ada watchlist :(",
+                    style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                    child: Container(
+                  margin: const EdgeInsets.all(8),
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(children: [
+                    Center(
+                      child: TextFormField(
+                        maxLines: 10,
+                        decoration: InputDecoration(
+                          hintText: "Masukkan Keluhan Anda...",
+                          labelText: "Keluhan",
+                          icon: const Icon(Icons.title),
+                          // Menambahkan icon agar lebih intuitif
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Masukkan Nomor Telepom",
+                          labelText: "No. HP",
+                          icon: const Icon(Icons.phone),
+                          // Menambahkan icon agar lebih intuitif
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: DropdownButton<DoctorReady>(
+                          value: snapshot.data[0]!,
+                          hint: const Text("Pilih Jenis"),
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          items: snapshot.data
+                              .map<DropdownMenuItem<DoctorReady>>(
+                                  (DoctorReady value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text(value.username),
+                            );
+                          }).toList(),
+                          onChanged: (DoctorReady? value) {
+                            setState(() {
+                              if (value?.username == null) {
+                                _selectDokter = "";
+                              } else {
+                                _selectDokter = value?.username;
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ]),
+                )),
+              );
+            }
+          }
+        });
   }
 }
