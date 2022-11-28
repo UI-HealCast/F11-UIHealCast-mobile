c823922f3900aa50edeba208041d1eab22d592a8
yudiptr <94793172+yudiptr@users.noreply.github.com>
2022-11-28 21:06:44 +0700

    sukses get data

--- lib/pelayananDokter/component/form_pelayanan_dokter.dart
+++ lib/pelayananDokter/component/form_pelayanan_dokter.dart
@@ -1,5 +1,6 @@
 import 'package:f11uihealcast/pelayananDokter/api/get_doctor_ready.dart';
 import 'package:f11uihealcast/pelayananDokter/model/doctor_ready.dart';
+import 'package:f11uihealcast/pelayananDokter/page/pelayanan_dokter.dart';
 import 'package:flutter/material.dart';
 
 class FormPelayananDokter extends StatefulWidget {
@@ -15,6 +16,18 @@ class _FormPelayananDokter extends State<FormPelayananDokter> {
   get formKey => widget.formKey;
 
   String? _selectDokter;
+  String? keluhan;
+  String? noHP;
+  DoctorReady? tempDokter;
+
+  bool isNumeric(String value) {
+    if (value == null) {
+      return false;
+    }
+    return int.tryParse(value) != null;
+  }
+
+  List<DoctorReady>? isReady;
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +48,7 @@ class _FormPelayananDokter extends State<FormPelayananDokter> {
                 ],
               );
             } else {
+              isReady = snapshot.data;
               return Form(
                 key: formKey,
                 child: SingleChildScrollView(
@@ -55,6 +69,19 @@ class _FormPelayananDokter extends State<FormPelayananDokter> {
                             borderRadius: BorderRadius.circular(5.0),
                           ),
                         ),
+                        onSaved: (String? value) {
+                          setState(() {
+                            keluhan = value!;
+                          });
+                        },
+                        // Validator sebagai validasi form
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Judul tidak boleh kosong!';
+                          }
+                          keluhan = value;
+                          return null;
+                        },
                       ),
                     ),
                     const SizedBox(
@@ -72,6 +99,23 @@ class _FormPelayananDokter extends State<FormPelayananDokter> {
                             borderRadius: BorderRadius.circular(5.0),
                           ),
                         ),
+                        keyboardType: TextInputType.number,
+                        // Menambahkan behavior saat data disimpan
+                        onSaved: (String? value) {
+                          setState(() {
+                            noHP = value;
+                          });
+                        },
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Nominal tidak boleh kosong!';
+                          }
+                          if (!isNumeric(value)) {
+                            return 'Nominal harus berupa angka!';
+                          }
+                          noHP = value;
+                          return null;
+                        },
                       ),
                     ),
                     const SizedBox(
@@ -80,30 +124,55 @@ class _FormPelayananDokter extends State<FormPelayananDokter> {
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Center(
-                        child: DropdownButton<DoctorReady>(
-                          value: snapshot.data[0]!,
+                        child: DropdownButton<String>(
+                          value: _selectDokter,
                           hint: const Text("Pilih Jenis"),
                           icon: const Icon(Icons.keyboard_arrow_down),
-                          items: snapshot.data
-                              .map<DropdownMenuItem<DoctorReady>>(
-                                  (DoctorReady value) {
+                          items: isReady!.map<DropdownMenuItem<String>>(
+                              (DoctorReady value) {
                             return DropdownMenuItem(
-                              value: value,
+                              value: value.pk.toString(),
                               child: Text(value.username),
                             );
                           }).toList(),
-                          onChanged: (DoctorReady? value) {
+                          onChanged: (String? value) {
                             setState(() {
-                              if (value?.username == null) {
+                              if (value == null) {
                                 _selectDokter = "";
                               } else {
-                                _selectDokter = value?.username;
+                                _selectDokter = value;
                               }
                             });
                           },
                         ),
                       ),
                     ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextButton(
+                      child: const Text(
+                        "Buat Janji",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      ),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          print(keluhan);
+                          print(_selectDokter);
+                          print(noHP);
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const PelayananDokterPage()),
+                          );
+                        }
+                        ;
+                      },
+                    )
                   ]),
                 )),
               );
